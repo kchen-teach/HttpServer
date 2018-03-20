bfb81382c6b88ee75b6ad171ec0d3d9f72f3d57a
kchen-teach <32706098+kchen-teach@users.noreply.github.com>
2018-03-20 21:49:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
-const port = 3000
+const port = process.env.port || 3000
 
 const webPath = 'public'
 
